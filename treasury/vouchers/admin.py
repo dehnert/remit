a96d938c05d3d73ed3a581ff9d4dc9c13f44c209
Alex Dehnert <adehnert@mit.edu>
2010-01-06 02:13:19 -0500

    Improve ReimbursementRequest admin area

--- treasury/vouchers/admin.py
+++ treasury/vouchers/admin.py
@@ -1,11 +1,31 @@
 import vouchers
 from django.contrib import admin
 
+indent_str = u"\u00A0\u00A0"
+
 class BudgetAreaAdmin(admin.ModelAdmin):
     pass
     #fields = [ 'path', 'name', 'comment', 'owner', 'interested', ]
 
+class ReimbursementRequestAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        field = super(ReimbursementRequestAdmin, self).formfield_for_foreignkey(
+                                                db_field, request, **kwargs)
+        print "In RRA"
+        if db_field.rel.to == vouchers.models.BudgetArea:
+            field.label_from_instance = self.get_budgetarea_name
+        return field
+
+    def get_budgetarea_name(self, area, ):
+        return indent_str*area.depth + unicode(area)
+
+    fieldsets = [
+        ('Request metadata', {'fields': ['submitter']}),
+        ('Recipient', {'fields': ['check_to_name', 'check_to_email', 'check_to_addr', ] }),
+        ('Expense details', {'fields': ['amount', 'budget_area', 'budget_term', ] }),
+    ]
+
 admin.site.register(vouchers.models.BudgetArea, BudgetAreaAdmin)
 admin.site.register(vouchers.models.BudgetTerm)
 admin.site.register(vouchers.models.BudgetAreaTerm)
-admin.site.register(vouchers.models.ReimbursementRequest)
+admin.site.register(vouchers.models.ReimbursementRequest, ReimbursementRequestAdmin)
--- treasury/vouchers/models.py
+++ treasury/vouchers/models.py
@@ -70,9 +70,9 @@ class BudgetAreaTerm(models.Model):
 
 class ReimbursementRequest(models.Model):
     submitter = models.CharField(max_length=10) # MIT username of submitter
-    check_to_name = models.CharField(max_length=50)
-    check_to_email = models.EmailField()
-    check_to_addr = models.TextField(blank=True)
+    check_to_name = models.CharField(max_length=50, verbose_name="check recipient's name")
+    check_to_email = models.EmailField(verbose_name="email address for check pickup")
+    check_to_addr = models.TextField(blank=True, verbose_name="address for check mailing", help_text="For most requests, this should be blank for pickup in SAO (W20-549)")
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     budget_area = models.ForeignKey(BudgetArea)
     budget_term = models.ForeignKey(BudgetTerm)
