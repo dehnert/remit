269db509f03d0ea14bb1ffa85958ae1e550702f7
Alex Dehnert <adehnert@mit.edu>
2010-01-06 04:03:55 -0500

    First pass at request submission

--- treasury/vouchers/urls.py
+++ treasury/vouchers/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 import treasury.vouchers.views
 
 urlpatterns = patterns('',
+    (r'submit/(?P<term>[\d\w-]+)/(?P<committee>[\d\w-]+)/', 'treasury.vouchers.views.submit_request', )
 )
--- treasury/vouchers/views.py
+++ treasury/vouchers/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+import vouchers.models
+from finance_core.models import BudgetTerm, BudgetArea
+
+from django.shortcuts import render_to_response
+from django.http import Http404
+
+def submit_request(request, term, committee):
+    try:
+        term_obj = BudgetTerm.objects.get(slug=term)
+    except BudgetTerm.DoesNotExist:
+        raise Http404
+    try:
+        comm_obj = BudgetArea.objects.get(pk=committee)
+    except BudgetArea.DoesNotExist:
+        raise Http404
+
+    context = {
+        'term':term_obj,
+        'comm':comm_obj,
+    }
+    return render_to_response('vouchers/submit.html', context)
