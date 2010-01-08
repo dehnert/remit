cafab4c685a24b1eecb281d0a0ef79e6aa543d74
Alex Dehnert <adehnert@mit.edu>
2010-01-08 04:49:02 -0500

    Serve static media

--- treasury/settings.py
+++ treasury/settings.py
@@ -1,7 +1,7 @@
 # Django settings for treasury project.
 import os
 
-APP_ROOT = os.path.dirname(__file__)
+SITE_ROOT = os.path.normpath(os.path.dirname(__file__))
 DEFAULT_DOMAIN = 'mit.edu'
 
 DEBUG = True
@@ -39,17 +39,18 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = SITE_ROOT + '/media/'
+print "MEDIA_ROOT='%s'" % (MEDIA_ROOT, )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '_poit_qs6s#!63a5pqn7nqa4q90eaupr6^@=u(symfvlz2c)n&'
@@ -75,7 +76,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    APP_ROOT + '/templates/',
+    SITE_ROOT + '/templates/',
 )
 
 INSTALLED_APPS = (
--- treasury/urls.py
+++ treasury/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.views import login
 
+import settings
+
 # Necessary views
 import finance_core.views
 
@@ -22,3 +24,14 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/login/', login, ),
 )
+
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+                                (r'^%s(?P<path>.*)$' % _media_url,
+                                serve,
+                                {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
