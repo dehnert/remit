e64b195dccf8dbb85b89c6db86f8f180a8da97a1
Alex Dehnert <adehnert@mit.edu>
2010-01-15 20:38:22 -0500

    Don't print the media URL

--- treasury/settings.py
+++ treasury/settings.py
@@ -40,7 +40,6 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = SITE_ROOT + '/media/'
-print "MEDIA_ROOT='%s'" % (MEDIA_ROOT, )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
