31bd056548b5b07969464d32b0c8a515e894d00f
Alex Dehnert <adehnert@mit.edu>
2010-01-22 04:56:42 -0500

    Add voucher support (WIP WIP WIP)

--- treasury/vouchers/models.py
+++ treasury/vouchers/models.py
@@ -11,8 +11,9 @@ class ReimbursementRequest(models.Model):
     check_to_email = models.EmailField(verbose_name="email address for check pickup")
     check_to_addr = models.TextField(blank=True, verbose_name="address for check mailing", help_text="For most requests, this should be blank for pickup in SAO (W20-549)")
     amount = models.DecimalField(max_digits=7, decimal_places=2, help_text='Do not include "$"')
-    budget_area = models.ForeignKey(BudgetArea)
+    budget_area = models.ForeignKey(BudgetArea, related_name='as_budget_area')
     budget_term = models.ForeignKey(BudgetTerm)
+    expense_area = models.ForeignKey(BudgetArea, related_name='as_expense_area') # ~GL
     request_time = models.DateTimeField(default=datetime.datetime.now)
     approval_time = models.DateTimeField(blank=True, null=True,)
     printing_time = models.DateTimeField(blank=True, null=True,)
@@ -29,3 +30,16 @@ class ReimbursementRequest(models.Model):
             self.budget_area.name,
             self.budget_term.name,
         )
+
+
+class Voucher(models.Model):
+    group_name = models.CharField(max_length=10)
+    account = models.IntegerField()
+    signatory = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_address = models.EmailField(max_length=50)
+    mailing_address = models.TextField()
+    amount = models.DecimalField(max_digits=7, decimal_places=2,)
+    description = models.TextField()
+    gl = models.IntegerField()
