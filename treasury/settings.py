32e360b9c9bc98325c64973c00fd3db7981fb085
Alex Dehnert <adehnert@mit.edu>
2010-01-29 06:02:43 -0500

    Create remit_templates; put base template there

--- treasury/settings.py
+++ treasury/settings.py
@@ -76,6 +76,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     SITE_ROOT + '/templates/',
+    SITE_ROOT + '/remit_templates/',
 )
 
 INSTALLED_APPS = (
