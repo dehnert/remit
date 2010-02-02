2ff0d60b243a75bf504058ca1ded53f20e0047e4
Alex Dehnert <adehnert@mit.edu>
2010-02-02 05:50:01 -0500

    Try to get it deployable to scripts

--- remit/settings.py
+++ remit/settings.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 SITE_ROOT = os.path.normpath(os.path.dirname(__file__))
+SITE_WEB_PATH = ''
 DEFAULT_DOMAIN = 'mit.edu'
 
 DEBUG = True
@@ -47,12 +48,12 @@ MEDIA_ROOT = SITE_ROOT + '/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = SITE_WEB_PATH + '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = SITE_WEB_PATH + '/media/admin/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -90,3 +91,5 @@ INSTALLED_APPS = (
     'vouchers',
     'finance_core',
 )
+
+from local_settings_after import *
