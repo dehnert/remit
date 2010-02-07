7df1c5075e753086cd3dafff7c8b77a1284dcc38
Alex Dehnert <adehnert@mit.edu>
2010-02-07 03:26:32 -0500

    Transaction in the admin UI

--- remit/finance_core/admin.py
+++ remit/finance_core/admin.py
@@ -10,10 +10,27 @@ class BudgetTermAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ('name', 'start_date', 'end_date', 'submit_deadline', )
 
+
 class BudgetAreaTermAdmin(admin.ModelAdmin):
     list_display = ('budget_area', 'budget_term', )
+
+
+class LineItemInline(admin.TabularInline):
+    model = finance_core.models.LineItem
+
+
+class LineItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    inlines = [
+        LineItemInline,
+    ]
 
 
 admin.site.register(finance_core.models.BudgetArea, BudgetAreaAdmin)
 admin.site.register(finance_core.models.BudgetTerm, BudgetTermAdmin)
 admin.site.register(finance_core.models.BudgetAreaTerm, BudgetAreaTermAdmin)
+admin.site.register(finance_core.models.Transaction, TransactionAdmin)
+admin.site.register(finance_core.models.LineItem, LineItemAdmin)
