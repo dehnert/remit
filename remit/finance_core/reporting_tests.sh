ae83758466f9c9de6d97b76481882efc0dfe429c
Alex Dehnert <adehnert@mit.edu>
2010-03-27 17:03:18 -0400

    Improved testing

--- remit/finance_core/reporting_tests.sh
+++ remit/finance_core/reporting_tests.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-baseurl=${1:-http://localhost:8001/finance_core/reporting/?term=2010-spring}
-dirname=$(mktemp -d /tmp/remit.XXXXXX)
 methods="annotate aggregate"
-cd $dirname
-wget -O default.html "$baseurl"
-for method in $methods; do
-    wget -O $method.html  "$baseurl&compute_method=$method"
-done
-for method in $methods; do
-    diff -q --report-identical-files default.html $method.html
+baseurl=${1:-http://localhost:8001/finance_core/reporting/?term=2010-spring}
+baseurls="$baseurl $baseurl&area=13 $baseurl&layer=30"
+for testbaseurl in $baseurls; do
+    echo $testbaseurl
+    dirname=$(mktemp -d /tmp/remit.XXXXXX)
+    cd $dirname
+    wget -O default.html "$baseurl"
+    for method in $methods; do
+        wget -q -O $method.html  "$baseurl&compute_method=$method"
+    done
+    for method in $methods; do
+        diff -q --report-identical-files default.html $method.html
+    done
 done
