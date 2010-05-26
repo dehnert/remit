a1e4224a4df5f999a9651cfa10ed9f770568178d
Alex Dehnert <adehnert@mit.edu>
2010-05-26 05:24:59 -0400

    Create a downloader user

--- remit/util/setup.py
+++ remit/util/setup.py
@@ -8,13 +8,15 @@ if __name__ == '__main__':
     sys.path.append(django_dir)
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+import settings
+
 import finance_core.models
 import vouchers.models
 
 import util.add_gl_accounts
 import finance_core.util
 
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
@@ -47,11 +49,21 @@ if __name__ == '__main__':
     # Do the various auth setup
     get_or_create_group('autocreated')
     get_or_create_group('mit')
+    local_auth_only = get_or_create_group('local-auth-only')
     treasurers = get_or_create_group('treasurers')
     treasurer_perms = Permission.objects.filter(content_type__app_label__in=['vouchers', 'finance_core', ],)
     for perm in treasurer_perms:
         treasurers.permissions.add(perm)
     treasurers.save()
-    downloader = get_or_create_group('downloader')
-    grant_by_codename(downloader, 'generate_vouchers', )
-    downloader.save()
+    gdownloader = get_or_create_group('downloader')
+    grant_by_codename(gdownloader, 'generate_vouchers', )
+    gdownloader.save()
+    try:
+        udown = User.objects.get(username='downloader', )
+    except User.DoesNotExist:
+        udown = User.objects.create_user(username='downloader', email=settings.SERVER_EMAIL, )
+        udown.is_active = False
+        udown.is_staff  = True
+        udown.groups.add(local_auth_only)
+        udown.groups.add(gdownloader)
+        udown.save()
