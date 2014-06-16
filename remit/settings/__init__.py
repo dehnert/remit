6479b5c0c38330fb22dc39de422f35bd08f97cf0
Alex Dehnert <adehnert@mit.edu>
2014-06-15 21:59:57 -0400

    Django 1.6: enable the staticfiles app
    Without it, admin media serving doesn't seem to work.

--- remit/settings/__init__.py
+++ remit/settings/__init__.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'treebeard',
     'south',
     'vouchers',
